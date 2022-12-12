426025f916350e863a8b4355894313c032930a1c
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-12 16:19:50 +0000

    Adding tarfile member sanitization to extractall()

--- protonfixes/util.py
+++ protonfixes/util.py
@@ -638,7 +638,26 @@ def install_all_from_tgz(url, path=os.getcwd()):
     
     with tarfile.open(tgz_file_path, 'r:gz') as tgz_obj:
         log.info('Extracting ' + tgz_file_name + ' to ' + path)
-        tgz_obj.extractall(path)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tgz_obj, path)
     
 def install_from_zip(url, filename, path=os.getcwd()):
     """ Install a file from a downloaded zip
