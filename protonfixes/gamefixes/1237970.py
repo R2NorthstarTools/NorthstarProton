d760476ff5c1d85e3a111a0725f63ce47592fde0
begin-theadventure <99835765+begin-theadventure@users.noreply.github.com>
2023-01-10 00:07:26 +0100

    Add dependencies for the EA App.
    liberation font and d3dcompiler_47.

--- protonfixes/gamefixes/1237970.py
+++ protonfixes/gamefixes/1237970.py
@@ -6,5 +6,5 @@ from protonfixes import util
 def main():
 
     util.set_environment('WINEDLLOVERRIDES','wsock32=n,b')
-
-
+    util.protontricks('liberation')
+    util.protontricks('d3dcompiler_47')
