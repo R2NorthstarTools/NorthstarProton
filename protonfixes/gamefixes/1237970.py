c66b9052cfad7fc56ca315706e49b3c382d9a39a
james-amarth <116909003+james-amarth@users.noreply.github.com>
2023-01-10 13:05:10 -0600

    de-borked util.protontricks('d3dcompiler_47)

--- protonfixes/gamefixes/1237970.py
+++ protonfixes/gamefixes/1237970.py
@@ -1,9 +1,13 @@
 """ Custom Gamefix for Northstar
 """
+#pylint: disable=C0103
 
 from protonfixes import util
 
 def main():
-
+    """
+    """
+    
+    #installs d3dcompiler_47 to make EA App render the GUI
     util.set_environment('WINEDLLOVERRIDES','wsock32=n,b')
     util.protontricks('d3dcompiler_47')
