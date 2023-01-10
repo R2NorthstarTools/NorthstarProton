22501d870468e363857a6e5080f982b779941618
james-amarth <116909003+james-amarth@users.noreply.github.com>
2023-01-10 13:10:07 -0600

    de-borked protontricks

--- protonfixes/gamefixes/1237970.py
+++ protonfixes/gamefixes/1237970.py
@@ -11,3 +11,4 @@ def main():
     #installs d3dcompiler_47 to make EA App render the GUI
     util.set_environment('WINEDLLOVERRIDES','wsock32=n,b')
     util.protontricks('d3dcompiler_47')
+    util.protontricks('liberation')
