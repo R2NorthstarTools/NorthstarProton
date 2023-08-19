c09caf5d601a3dfb4325e7f3dac92cd526619216
GloriousEggroll <gloriouseggroll@gmail.com>
2023-08-19 15:28:56 -0600

    Revert "patches: add fix for steam helper ignoring command line arguements, notably in Northstar (PR #6555)"
    This reverts commit 44549b7679c00a418393059be743abbb31c57d07.
    
    This breaks Escape from Monkey Island and Memento Mori

--- patches/protonprep-valve-staging.sh
+++ patches/protonprep-valve-staging.sh
@@ -10,14 +10,8 @@
     cd vkd3d-proton
     git reset --hard HEAD
     git clean -xdf
+    
     cd ..
-
-    # https://github.com/ValveSoftware/Proton/pull/6555
-    cd steam_helper
-    git checkout steam.cpp
-    cd ..
-    patch -Np1 < ./patches/steam_helper/6555.patch
-
 
 ### END PREP SECTION ###
 
