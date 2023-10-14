5a1700c3048206248bc62acc7944ac5c768c196e
GloriousEggroll <gloriouseggroll@gmail.com>
2023-10-14 13:40:32 -0600

    patches: add fsr resolution calculation fixup

--- patches/protonprep-valve-staging.sh
+++ patches/protonprep-valve-staging.sh
@@ -355,6 +355,9 @@
     echo "WINE: -FSR- fullscreen hack fsr patch"
     patch -Np1 < ../patches/proton/48-proton-fshack_amd_fsr.patch
     
+    echo "WINE: -FSR- fullscreen hack resolution calculation fixup"
+    patch -Np1 < ../patches/proton/49-fsr-width-using-height-and-aspect-ratio.patch
+    
     #echo "WINE: -FSR- enable FSR flag by default (fixes broken fs hack scaling in some games like Apex and FFXIV)"
     #patch -Np1 < ../patches/proton/71-invert-fsr-logic.patch
 
